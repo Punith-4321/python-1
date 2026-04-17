0eee0fd62a88f15c4eb0826ce216482642f22e22
Punith-4321 <punithceo07@gmail.com>
2026-04-17 08:04:06 +0530

    Add files via upload

---
+++ python program for identiy operators with intigers.py
@@ -0,0 +1,9 @@
+# Identity operators example with integers
+
+a = 10
+b = 10
+c = 20
+
+print(a is b)      # True (same value, same object in many cases)
+print(a is c)      # False
+print(a is not c)  # True
